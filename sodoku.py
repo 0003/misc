9f5e70f3490d9c17ca33d3a4f38cf5eac2b026b5
Will <will@new-host-3.home>
2014-07-01 00:22:21 -0400

    Updated just before def parse_grid

--- sodoku.py
+++ sodoku.py
@@ -22,7 +22,7 @@ print squares
 print squaresWL
 print timeit.timeit("cross(rows,cols)",number=1000, setup= "from __main__ import cross, rows, cols")
 print timeit.timeit("crossWL(rows,cols)",number=1000, setup= "from __main__ import cross, crossWL, rows, cols")
-
+print len(squares)
 #Norvigs cross-product function runs faster.
 
 
@@ -39,4 +39,30 @@ list_of_units = [cross(rows,column_number) for column_number in cols] +\
 print "printing list of units \n"
 for n, l in enumerate(list_of_units):
     print n, l
+
+""" units is a dict that maps a square to all of its units """
+
+units = dict( (square,[unit for unit in list_of_units if square in unit])
+               for square in squares)
+print '\n printing unit test'
+print units['A1']
+print len(units['A1'])
+
+"""peers is a dict that maps a square to all of its peer squares """
+
+peers = dict( (square,set(sum(units[square],[])) - set([square])) for square in squares)
+
+print '\n printing peer test'
+print peers['A1']
+print len(peers['A1'])
+print set(sum(units['A1'],[])) - set(['A1'])
+print sum(units['A1'],[])
+print sum(units['A1'])
+
+
+""" Grid is the input that represents the inital state
+    Values are the intermediate status of the grid that we will change. This will be a dict with the key
+    being a square - ie 'A1' -- and the values being a string of remaining digits - ie '123456'"""
+
+
 
