61bee1b9c94d1e9bc82bde09c16e1bc2584063b9
Will Leach <wleach@Wills-MacBook-Pro.local>
2014-07-15 15:37:53 -0400

    wtf now prints a random wtf

--- wtf.py
+++ wtf.py
@@ -1,7 +1,20 @@
 __author__ = 'wleach'
+import random
+
+MESSAGE = 'wtf'
 
 with open('/usr/share/dict/words','r') as f:
-    words = f.read()
-words = [ word for word in words if all([word[0].lower() in 'abc', len(word)>3])]
+    words = f.readlines()
 
-print words
+word_dict = dict((c, []) for c in MESSAGE)
+print word_dict
+words = [ ''.join(word.split()) for word in words if all([word[0].lower() in MESSAGE, len(word)>3])]
+
+for word in words:
+    word_dict[word[0].lower()] += [word]
+
+print word_dict
+
+wtf = random.choice(word_dict['w']) + ' ' + random.choice(word_dict['t']) + ' ' + random.choice(word_dict['f'])
+
+print wtf
