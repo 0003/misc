de13ef1ee5e8bf63b825d13f0f953f8051aebf8a
0003 <leach87@gmail.com>
2014-12-04 21:54:49 -0500

    added logger.py

---
+++ logger.py
@@ -0,0 +1,14 @@
+__author__ = 'will'
+
+import sleekxmpp
+import sys
+from optparse import OptionParser
+
+def main():
+    opt = OptionParser()
+    
+
+if __name__ == "__main__":
+    main()
+else:
+    main()
